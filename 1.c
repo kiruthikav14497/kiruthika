c4b3dae7319008d3d0412cee70c14fe4208d0241
kiruthikav14497 <kiruthika.vece@ksrce.ac.in>
2017-06-06 15:25:30 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the value a");
+scanf("%d",&a);
+if(a<0)
+{
+printf("a is the Negative number");
+}
+else if(a>0)
+{
+printf("a is the Positive number");
+}
+else
+{
+printf("a is the Zero");
+}
+}
