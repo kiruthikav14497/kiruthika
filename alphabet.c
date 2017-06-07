2169c9c9c5838ac800c0e8c6c3e612dbc39a911b
kiruthikav14497 <kiruthika.vece@ksrce.ac.in>
2017-06-07 12:18:37 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,17 @@
+#include<conio.h>
+#include<string.h>
+#include<stdio.h>
+int main();
+{
+char c;
+printf("Enter the c");
+scanf("%s",&c);
+if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+printf("c is the Alphabet");
+else
+printf("c is the numbers");
+return 0;
+}
+
+
+
