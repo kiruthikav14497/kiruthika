c1a6bd26802e870251659bbd7d11bd8546d68423
kiruthikav14497 <kiruthika.vece@ksrce.ac.in>
2017-07-04 11:45:43 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,28 @@
+package prp;
+import java.util.Scanner;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer that you would like to have reversed: ");
+        int number = input.nextInt();
+
+        reverse(number);
+    }
+
+    public static void reverse(int userInteger)
+    {
+        int tempDigit = 0;
+
+        while (userInteger > 0){
+
+            tempDigit = userInteger % 10;
+            System.out.print(tempDigit);
+            userInteger = userInteger / 10;
+        }
+
+	}
+
+}
