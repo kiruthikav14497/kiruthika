fda08192e08bbe0904c9e8a984d07683f6da8a2d
kiruthikav14497 <kiruthika.vece@ksrce.ac.in>
2017-07-04 11:41:14 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,29 @@
+package prp;
+import java.util.Scanner;
+class Numbers {
+
+	public static void main(String[] args) {
+		 int no,a=0;
+		 Scanner s=new Scanner(System.in);
+		System.out.println("Enter any number : ");
+		 no = s.nextInt();
+
+		if(no<0)
+		{
+		no=no * -1;
+		}
+		else if(no==0)
+		{
+		no=1;
+		}
+		while(no>0)
+		{
+		no=no/10;
+		a++;
+		}
+		System.out.println("Number of Digits in given number is: "+a);
+
+
+	}
+
+}
