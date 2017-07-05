f596ca4930b7d77e2ae90bcdfde2c245dc393772
kiruthikav14497 <kiruthika.vece@ksrce.ac.in>
2017-07-05 11:43:11 +0530

    Add files via upload

---
+++ CamelCase.java
@@ -0,0 +1,23 @@
+package prp;
+
+public class CamelCase {
+
+	public static void main(String[] args) {
+		   if (init==null)
+		        return null;
+
+		    final StringBuilder ret = new StringBuilder(init.length());
+
+		    for (final String word : init.split(" ")) {
+		        if (!word.isEmpty()) {
+		            ret.append(word.substring(0, 1).toUpperCase());
+		            ret.append(word.substring(1).toLowerCase());
+		        }
+		        if (!(ret.length()==init.length()))
+		            ret.append(" ");
+		    }
+
+		    return ret.toString();
+	}
+
+}
