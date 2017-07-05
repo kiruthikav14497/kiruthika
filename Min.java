ae5311ca4ce54b668801fc6fbcf393b03984ea17
kiruthikav14497 <kiruthika.vece@ksrce.ac.in>
2017-07-05 11:51:06 +0530

    Add files via upload

---
+++ Min.java
@@ -0,0 +1,15 @@
+package prp;
+
+public class Min {
+
+	public static void main(String[] args) 
+		throws Exception {  
+		    long time = System.currentTimeMillis();
+		    DateTime time1 = new DateTime(time);
+		    DateTime time2 = new DateTime(time + 120_000); 
+		    Minutes minutes = Minutes.minutesBetween(time1, time2);
+		    System.out.println(minutes.getMinutes()); 
+
+	}
+
+}
