11a430ee845034087a09035878c97df1f091d2d3
kiruthikav14497 <kiruthika.vece@ksrce.ac.in>
2017-07-07 10:27:15 +0530

    Create vowellls.java

---
+++ vowellls.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class JavaProgram
+{
+   public static void main(String args[])
+   {
+       String strOrig, strNew;
+       Scanner scan = new Scanner(System.in);
+       System.out.print("Enter a String : ");
+       strOrig = scan.nextLine();
+       System.out.print("Removing Vowels from The String [" +strOrig+ "]\n");
+       strNew = strOrig.replaceAll("[aeiouAEIOU]", "");
+       System.out.print("All Vowels Removed Successfully..!!\nNow the String is :\n");    
+       System.out.print(strNew);
+   }
+}
